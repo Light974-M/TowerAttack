9aa75fc0c3f5225b501620c239d12f3352be55a2
maximilien vitiello <maxiquick@outlook.fr>
2021-03-03 15:53:33 +0100

    push de pierre

---
+++ TowerAttack_Engine/Assets/Scripts/Datas/SquareData.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System;
+
+public enum SquareState
+{
+    Normal,
+    Wall,
+    Water,
+}
+
+public enum Alignment
+{
+    Player,
+    IA,
+    Neutral
+}
+
+[Serializable]
+public struct SquareData
+{
+    [SerializeField]
+    private SquareState _state;
+
+    public SquareState State
+    {
+        get { return _state; }
+        set { _state = value; }
+    }
+
+    [SerializeField]
+    private Alignment _alignment;
+
+    public Alignment Alignment
+    {
+        get { return _alignment; }
+        set { _alignment = value; }
+    }
+
+    public static Color GetColorFromState(SquareState state)
+    {
+        switch (state)
+        {
+            case SquareState.Wall:
+                return Color.black;
+            case SquareState.Water:
+                return Color.cyan;
+            default:
+                return Color.green;
+        }
+    }
+
+    public static Color GetColorFromAlignment(Alignment alignment)
+    {
+        switch (alignment)
+        {
+            case Alignment.Player:
+                return Color.blue;
+            case Alignment.IA:
+                return Color.red;
+            default:
+                return Color.white;
+        }
+    }
+}
---
+++ TowerAttack_Engine/Assets/Scripts/Managers/MapManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class MapManager : MonoBehaviour
+{
+    [SerializeField]
+    private MapData _mapData;
+
+    public MapData MapData { get { return _mapData; } }
+
+    public bool displayDebugView = true;
+
+    [ContextMenu("GenerateMap")]
+    public void GenerateMap()
+    {
+        _mapData.ResetMap();
+
+        //_mapData.GenSquareRandom();
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (displayDebugView)
+        {
+            Vector3 sizeSquareMap = new Vector3(0.8f, 0.2f, 0.8f);
+            for (int i = 0; i < _mapData.Width; i++)
+            {
+                for (int j = 0; j < _mapData.Height; j++)
+                {
+                    SquareData currentSquareData = _mapData.GetSquareData(i, j);
+
+                    // State Square
+                    Gizmos.color = SquareData.GetColorFromState(currentSquareData.State);
+                    Gizmos.DrawWireCube(new Vector3(i + 0.5f, 0, j + 0.5f), sizeSquareMap);
+
+                    // Alignment
+                    Gizmos.color = SquareData.GetColorFromAlignment(currentSquareData.Alignment);
+                    Gizmos.DrawSphere(new Vector3(i + 0.75f, 0.1f, j + 0.75f), 0.1f);
+                }
+            }
+        }
+    }
+}
